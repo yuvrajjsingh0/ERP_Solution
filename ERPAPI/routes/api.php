ab403b42388b77b553d760ac1d4e55e29bf00b52
Yuvraj Singh <yuvrajjsingh0@gmail.com>
2023-08-11 03:18:01 +0530

    API: Added Routes for Clients and Payments

--- ERPAPI/routes/api.php
+++ ERPAPI/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\PaymentsController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Routes for Clients 
+Route::get('/clients', [ClientsController::class, 'index']);
+Route::get('/clients/{id}', [ClientsController::class, 'show']);
+Route::post('/clients', [ClientsController::class, 'store']);
+Route::put('/clients/{id}', [ClientsController::class, 'update']);
+Route::delete('/clients/{id}', [ClientsController::class, 'destroy']);
+
+// Routes for Payments 
+Route::get('/payments', [PaymentsController::class, 'index']);
+Route::get('/payments/{id}', [PaymentsController::class, 'show']);
+Route::post('/payments', [PaymentsController::class, 'store']);
+Route::put('/payments/{id}', [PaymentsController::class, 'update']);
+Route::delete('/payments/{id}', [PaymentsController::class, 'destroy']);
